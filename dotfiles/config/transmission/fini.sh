e69d086c621930330d9d8b4c297e93826540f077
Ari Archer <asnojus039@gmail.com>
2021-08-30 17:17:54 +0300

    gentoo

--- dotfiles/config/transmission/fini.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm -rf ~/.config/transmission/torrents ~/.config/transmission/resume
-
--- dotfiles/fish/functions/tarc.fish
+++
@@ -1,9 +0,0 @@
-# Defined in ./fish_funcs_add.fish @ line 40
-function tarc
-    if count $argv > /dev/null
-        /usr/bin/tar -czvf $argv[1] $argv[2..-1]
-    else
-        echo "tarc - compress folders into .tar.gz files"
-        echo "usage: <name> <folder(s)>"
-    end
-end
