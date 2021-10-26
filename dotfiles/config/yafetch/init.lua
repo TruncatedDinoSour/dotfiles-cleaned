75069ddc5ebbb8a4786c1b35bca42b436e8677a6
Ari Archer <truncateddinosour@gmail.com>
2021-10-26 03:50:26 +0300

    update @ Tue 26 Oct 03:50:26 EEST 2021

--- dotfiles/config/yafetch/init.lua
+++ dotfiles/config/yafetch/init.lua
@@ -41,23 +41,54 @@ else
     distro_icon = " "
 end
 
-local ascii1 = wht .. " ,d88b.d88b,    " .. res
-local ascii2 = red .. " 88888888888    " .. res
-local ascii3 = grn .. " `Y8888888Y´    " .. res
-local ascii4 = yel .. "   `Y888Y´      " .. res
-local ascii5 = blu .. "     `Y´        " .. res
+if os.getenv("USER") == "root" then
+    --[
+    -- Dear future me,
+    -- I am very sorry for the bad formatting,
+    -- I cannot make it any prettier :(
+    -- I hope you're not too mad about it...
+    --]
+
+    ascii1 = grn .. "    \\^V//       "  .. red .. bld
+    ascii2 = res .. "    |. .|       "   .. res
+    ascii3 = res .. "  - \\ - / _     "  .. res
+    ascii4 = res .. "   \\_| |_/      "  .. res
+    ascii5 = res .. "     \\ \\        " .. res
+    ascii6 = res .. "   "                .. red .. "__" .. res .. "/_/" .. red .. "__      "   .. res
+    ascii7 = red .. "  |_______|     "   .. res
+    ascii8 = red .. "   \\     /      "  .. res .. "I am (g)root, don't break me"              .. res
+    ascii9 = red .. "    \\___/       "  .. res
+
+    root_mode = true
+    root_extras = {
+        [1] = ascii6,
+        [2] = ascii7,
+        [3] = ascii8,
+        [4] = ascii9
+    }
+else
+    ascii1 = wht .. " ,d88b.d88b,    " .. res
+    ascii2 = red .. " 88888888888    " .. res
+    ascii3 = grn .. " `Y8888888Y´    " .. res
+    ascii4 = yel .. "   `Y888Y´      " .. res
+    ascii5 = blu .. "     `Y´        " .. res
+end
 
 yafetch.header_sep = string.format("%s@%s", blu, res)
 yafetch.header_sep_color = bold
 yafetch.header_format = ascii1 -- could be ascii1, an icon, etc.
 
 function yafetch.init()
-    -- print("")
     yafetch.header()
     yafetch.format(ascii2 .. res .. red, distro_icon, wht, distro)
     yafetch.format(ascii3 .. res .. grn, shell_icon, wht, shell)
     yafetch.format(ascii4 .. res .. yel, kernel_icon, wht, kernel)
     yafetch.format(ascii5 .. res .. blu, pkgs_icon, wht, pkgs)
-    -- print("")
+
+    if root_mode then
+        for _, extra_art in ipairs(root_extras) do
+            print(extra_art)
+        end
+    end
 end
 
